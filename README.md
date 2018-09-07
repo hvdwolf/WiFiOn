de5bb5d40e9ad26b21f11c1b99ab84cc5b94d5da
hvdwolf <hvdwolf@gmail.com>
2018-09-07 18:29:18 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# WiFiOn
-micro apk which does nothing else than switching WiFi on
+# WiFiOnApp
+
+This is for any android device.
+
+This mini apk does nothing more then programmatically switchon the WiFi. It can be used from the XFytTweaker for example in the ACC_ON section to make sure WiFi is alwasy on afer deep-sleep.
+
+See also https://github.com/hvdwolf/WiFiOff
---
+++ app/src/main/java/org/hvdw/wifion/WiFiOn.java
@@ -0,0 +1,30 @@
+package org.hvdw.wifion;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.content.Context;
+import android.net.wifi.WifiManager;
+
+public class WiFiOn extends Activity {
+    public static final String TAG = "WiFiOnApp";
+    private static Context mContext;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getApplicationContext();
+
+        Log.d(TAG, "Started WiFiOnApp; in OnCreate void");
+
+        WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE); 
+        wifiManager.setWifiEnabled(true);
+        //wifiManager.setWifiEnabled(false);
+
+        Log.d(TAG, "Switch On WiFi");
+
+        finish();
+    }
+
+}
